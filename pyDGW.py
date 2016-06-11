79e580ec337d0444ddf464f1e03feda89ae16303
Brig Young <brig@sonophotostudios.com>
2016-06-11 10:07:33 -0700

    Defined interface to directed graph walker

--- pyDGW.py
+++ pyDGW.py
@@ -10,9 +10,10 @@
               |_|    |___/                        
                 "Follow the Arrows until the end..."
 
-   FILENAME: pyDGW.py AUTHOR: BrigYoung 
+   FILENAME: pyDGW.py 
+     AUTHOR: "Brig Young, https://github.com/Sonophoto/"
     PURPOSE: "Implements a basic state machine architecture"               
-  COPYRIGHT: "2016 Brig Young, https://github.com/Sonophoto" 
+  COPYRIGHT: "2016 Brig Young, http://SonophotoStudios.com/" 
     LICENSE: "BSD 2-Clause, See LICENSE file in project root"         
 
   ASCII_ART: Jennifer E. Swofford
@@ -28,3 +29,38 @@ ___________________/  /__/  /__/  /__/  /________________________________
 """
 
 class DGW:
+   """Maintains a list of Nodes in a graph with the callback functions
+      that define the edges and possible transistions from one node to
+      the next. implements methods for adding nodes, setting nodes to 
+      be ending or starting nodes, and then walking thru the nodes by
+      calling the callback function of each node it enters beginning 
+      with the start node until it enters an end node."""
+
+
+
+    def __init__(self):
+        """constructor for the python Directed Graph Walker
+           Each node (or state/vertex) has a callback, and it is
+           the callback that defines the edges"""
+
+
+
+    def addNode(self, name, callback):
+        """Adds a node and its callback to our nodelist. This 
+           is the actual node, the callbacks define the edges"""
+
+
+
+    def setStartNode(self, name):
+        """Sets a node to be a startNode for the graph walker"""
+
+
+
+    def setEndNode(self, name):
+        """Sets a node to be an endNode for the graph walker"""
+
+
+
+    def run(self, node_data):
+        """Confirms model has a startNode and and endNode and 
+           begins an event loop on the startNode."""
