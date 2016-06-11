ba279928445f894ea712fac94885ce75cbee379a
Brig Young <brig@sonophotostudios.com>
2016-06-11 09:29:09 -0700

    adding class file with header

---
+++ pyDGW.py
@@ -0,0 +1,30 @@
+"""
+*********************************************************************
+    Python
+     -3.5-
+                             ____     ____  _        _
+               _ __  _   _  |  _ \   / ___|| \      / |
+              | '_ \| | | | | | | | | |  _  \ \ /\ / / 
+              | |_) | |_| | | |_| | | |_| |  \ V  V /  
+              | .__/ \__, | |____/   \____|   \_/\_/   
+              |_|    |___/                        
+                "Follow the Arrows until the end..."
+
+   FILENAME: pyDGW.py AUTHOR: BrigYoung 
+    PURPOSE: "Implements a basic state machine architecture"               
+  COPYRIGHT: "2016 Brig Young, https://github.com/Sonophoto" 
+    LICENSE: "BSD 2-Clause, See LICENSE file in project root"         
+
+  ASCII_ART: Jennifer E. Swofford
+                      __    __    __    __
+                     /  \  /  \  /  \  /  \
+____________________/  __\/  __\/  __\/  __\_____________________________
+___________________/  /__/  /__/  /__/  /________________________________
+                   | / \   / \   / \   / \  \____
+                   |/   \_/   \_/   \_/   \    o \
+                                           \_____/--<
+
+*********************************************************************
+"""
+
+class DGW:
