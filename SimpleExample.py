a3fa8da4169a3ac52e935571020de44757a7ab55
Brig Young <brig@sonophotostudios.com>
2017-10-26 20:32:21 -0700

    De-Crufting and Refining SimpleExample

--- SimpleExample.py
+++ SimpleExample.py
@@ -1,30 +1,43 @@
-"""Simplest example of a directed graph walker"""
+"""Simplest example of a directed graph walker
+   Copyright 2016, 2017 Brig Young, Sonophotostudios.com
+   License: BSD-2c, (Citation Required) See LICENSE file
+"""
 
 import pyDGW
 
 DGW_Simple = pyDGW.DGWalker()
 
 DGW_state = pyDGW.DGW_data()
-number = 0 
+last_state = 0                            # This is our "state" or "status"
 
 def DGWOP_start(DGW_state):
-   """Our start state initializes DGW_state.count = 1"""
-   print("We have reached The Beginning")
-   DGW_state.number = 1
-   operator = "stop"
-   print("State Number is now:", DGW_state.number)
-   print("Next Operator is:", operator)
-   return(operator, DGW_state)
+   """Our start state
+   """
+   print("We have entered start")         # Optional messaging
+   DGW_state.last_state = 1               # Do something to change status
+   if DGW_state.last_state :              # Use logic to determine next state 
+      operator = "stop"
+
+   print("Next Operator is:", operator)   # Optional messaging
+   return(operator, DGW_state)            # Pass modified state back to DGWalker
 
 def DGWOP_stop(DGW_state):
-   """Exits the state machine and prints a message""" 
-   print("We have reached The End")
+   """Our stop state
+   """
+   DGW_state.last_state = 42 
+   print("We have stopped")
+   return(DGW_state)                      # Returning our completed "output"
 
-DGW_Simple.DEBUG = True
-DGW_Simple.addNode("start", DGWOP_start)
+
+DGW_Simple.DEBUG = True                   # Setup any desired runtime options
+DGW_Simple.addNode("start", DGWOP_start)  # Add a callback for each state node
 DGW_Simple.addNode("stop", DGWOP_stop)
-DGW_Simple.setStartNode("start")
-DGW_Simple.setEndNode("stop")
-DGW_Simple.run(DGW_state)
+DGW_Simple.setStartNode("start")          # Define ONE starting node
+DGW_Simple.setEndNode("stop")             # Define ONE or MORE ending nodes
+DGW_Simple.run(DGW_state)                 # Run the initialized graph walker
 
 
+# Now you can do additional processing on DGW_state, generate other output
+# with matplotlib, and or format the information and print it on screen:
+ 
+print("The Answer Is: ", DGW_state.last_state)
