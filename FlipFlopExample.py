aad8d4ab9225e29922036c7dedb3709d8540eafd
Sonophoto <brig@sonophotostudios.com>
2020-12-20 23:59:39 -0800

    Fixed naming of state variable to be consistent with purpose

--- FlipFlopExample.py
+++ FlipFlopExample.py
@@ -22,10 +22,10 @@ import pyDGW
 
 DGW_FlipFlop = pyDGW.DGWalker()
 
-DGW_state = pyDGW.DGW_data()
-DGW_state.state = 0
-DGW_state.counter = 0
-DGW_state.limit = 20
+FF_state = pyDGW.DGW_data()
+FF_state.state = 0
+FF_state.counter = 0
+FF_state.limit = 10
 
 # Set to True to get debugging output from the graph walker 
 DGW_FlipFlop.KDEBUG = False 
@@ -34,48 +34,48 @@ DGW_FlipFlop.DEBUG = True
 # Set to True to get operating status messages from pyDGW
 DGW_FlipFlop.SDEBUG = False
 
-def DGWOP_start(DGW_state):
+def DGWOP_start(FF_state):
    """Our start state initializes our flip flop"""
    print("Beginning FlipFlopping...")
-   if DGW_FlipFlop.DEBUG: print("Count is: ", DGW_state.counter)
-   DGW_state.state = 0
+   if DGW_FlipFlop.DEBUG: print("Count is: ", FF_state.counter)
+   FF_state.state = 0
    operator = "flip"
-   return(operator, DGW_state)
+   return(operator, FF_state)
 
 
-def DGWOP_flip(DGW_state):
+def DGWOP_flip(FF_state):
    """We are Flip, if toggled we flop"""
-   if DGW_state.counter == DGW_state.limit: DGW_state.state = 2
-   if DGW_state.state == 0:
-      DGW_state.state =  1
-      DGW_state.counter += 1
+   if FF_state.counter == FF_state.limit: FF_state.state = 2
+   if FF_state.state == 0:
+      FF_state.state =  1
+      FF_state.counter += 1
       operator = "flop"
       if DGW_FlipFlop.DEBUG:
          print("Flip is Flopping")
-         print("Count is: ", DGW_state.counter)
-   if DGW_state.state == 2:
+         print("Count is: ", FF_state.counter)
+   if FF_state.state == 2:
       operator = "stop"
-   return(operator, DGW_state)
+   return(operator, FF_state)
 
 
-def DGWOP_flop(DGW_state):
+def DGWOP_flop(FF_state):
    """We are Flop, if toggled we flip"""
-   if DGW_state.counter == DGW_state.limit: DGW_state.state = 2
-   if DGW_state.state == 1:
-      DGW_state.state =  0
-      DGW_state.counter += 1
+   if FF_state.counter == FF_state.limit: FF_state.state = 2
+   if FF_state.state == 1:
+      FF_state.state =  0
+      FF_state.counter += 1
       operator = "flip"
       if DGW_FlipFlop.DEBUG:
          print("Flop is Flipping")
-         print("Count is: ", DGW_state.counter)
-   if DGW_state.state == 2:
+         print("Count is: ", FF_state.counter)
+   if FF_state.state == 2:
       operator = "stop"
-   return(operator, DGW_state)
+   return(operator, FF_state)
 
 
-def DGWOP_stop(DGW_state):
+def DGWOP_stop(FF_state):
    """Exits the state machine, allows us to output data""" 
-   print("We have executed ", DGW_state.counter, " Flip Flops")
+   print("We have executed ", FF_state.counter, " Flip Flops")
 
 DGW_FlipFlop.addNode("start", DGWOP_start)
 DGW_FlipFlop.addNode("flip", DGWOP_flip)
@@ -83,6 +83,6 @@ DGW_FlipFlop.addNode("flop", DGWOP_flop)
 DGW_FlipFlop.addNode("stop", DGWOP_stop)
 DGW_FlipFlop.setStartNode("start")
 DGW_FlipFlop.setEndNode("stop")
-DGW_FlipFlop.run(DGW_state)
+DGW_FlipFlop.run(FF_state)
 
 
