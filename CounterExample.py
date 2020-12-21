cc39950083e29adfe343fa7278ce2e2c4fe98379
Sonophoto <brig@sonophotostudios.com>
2020-12-21 00:03:39 -0800

    Fixed naming of state variables to be consistent with purpose

--- CounterExample.py
+++ CounterExample.py
@@ -4,41 +4,41 @@ import pyDGW
 
 DGW_Counter = pyDGW.DGWalker()
 
-DGW_state = pyDGW.DGW_data()
-DGW_state.count = 0
-DGW_state.limit = 20
+counter_state = pyDGW.DGW_data()
+counter_state.count = 0
+counter_state.limit = 10
 
 # Set to True to get debugging output from the graph walker 
 DGW_Counter.DEBUG = True
 
-def DGWOP_start(DGW_state):                               # Create a Start state that initializes
-   """Our start state initializes DGW_state.count = 0"""  # your state machine with data.
-   DGW_state.count = 0
+def OP_start(counter_state):                               # Create a Start state that initializes
+   """Our start state initializes counter_state.count = 0"""  # your state machine with data.
+   counter_state.count = 0
    operator = "counter"
-   return(operator, DGW_state)
+   return(operator, counter_state)
 
 
-def DGWOP_counter(DGW_state):                             # Add nodes that process your data and
+def OP_counter(counter_state):                             # Add nodes that process your data and
    """Our counter increments while .count < .limit"""     # determine the next node to move to
-   if DGW_state.count < DGW_state.limit:
-      DGW_state.count += 1
-      print("Counter has reached:", DGW_state.count)
+   if counter_state.count < counter_state.limit:
+      counter_state.count += 1
+      print("Counter has reached:", counter_state.count)
       operator = "counter"
-   if DGW_state.count == DGW_state.limit:
+   if counter_state.count == counter_state.limit:
       operator = "stop"
-   return(operator, DGW_state)
+   return(operator, counter_state)
 
 
-def DGWOP_stop(DGW_state):                                # Add one or more end nodes that do
+def OP_stop(counter_state):                                # Add one or more end nodes that do
    """Exits the state machine and prints our count"""     # final processing on your data and
-   print("We have reached", DGW_state.count)              # generate output on your display
+   print("We have reached", counter_state.count)              # generate output on your display
 
-DGW_Counter.addNode("start", DGWOP_start)                 # Finally use methods from the DGWalker
-DGW_Counter.addNode("counter", DGWOP_counter)             # class to setup the machinery for your 
-DGW_Counter.addNode("stop", DGWOP_stop)                   # state machine and run it
+DGW_Counter.addNode("start", OP_start)                 # Finally use methods from the DGWalker
+DGW_Counter.addNode("counter", OP_counter)             # class to setup the machinery for your 
+DGW_Counter.addNode("stop", OP_stop)                   # state machine and run it
 DGW_Counter.setStartNode("start")
 DGW_Counter.setEndNode("stop")
-DGW_Counter.run(DGW_state)
+DGW_Counter.run(counter_state)
 
 """ You can do additional processing on your DGW_state object here 
     after your state machine has shut down
