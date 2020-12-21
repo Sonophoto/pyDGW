6ed751127f89bbe257d65a4d40d2b08312207c1c
Sonophoto <brig@sonophotostudios.com>
2020-12-21 00:18:45 -0800

    Fixed formmatting, additional explanatory comments

--- SimpleExample.py
+++ SimpleExample.py
@@ -1,11 +1,11 @@
 """
 *************************************************************************   
-     ____  _                 _          _____ ____  __  __ 
-    / ___|(_)_ __ ___  _ __ | | ___    |  ___/ ___||  \/  |
-    \___ \| | '_ ` _ \| '_ \| |/ _ \   | |_  \___ \| |\/| |
-     ___) | | | | | | | |_) | |  __/   |  _|  ___) | |  | |
-    |____/|_|_| |_| |_| .__/|_|\___|   |_|   |____/|_|  |_|
-                      |_|                                
+       ____  _                 _          _____ ____  __  __ 
+      / ___|(_)_ __ ___  _ __ | | ___    |  ___/ ___||  \/  |
+      \___ \| | '_ ` _ \| '_ \| |/ _ \   | |_  \___ \| |\/| |
+       ___) | | | | | | | |_) | |  __/   |  _|  ___) | |  | |
+      |____/|_|_| |_| |_| .__/|_|\___|   |_|   |____/|_|  |_|
+                        |_|                                
 
    FILENAME:  SimpleExample.py
    AUTHOR:    "Brig Young, https://github.com/Sonophoto/"
@@ -16,40 +16,50 @@
 *************************************************************************   
 """
 
-import pyDGW                              # Import the pyDGW module
+import pyDGW                             # Import the pyDGW module
 
-DGW_Simple = pyDGW.DGWalker()             # Instantiate a DGWalker object
+# First we create the state variable that we pass around. 
+# This is a trivial example but important
 
-DGW_state = pyDGW.DGW_data()              # Instatiate a DGW_data object
-DGW_state.the_answer = 0                  # Extend DGW_state with data members 
+Simple_state = pyDGW.DGW_data()          # Instatiate a DGW_data object
+Simple_state.the_answer = 0              # Extend Simple_state with data members 
 
-def DGWOP_start(DGW_state):               # A Start Node is required
-   """Our start state"""                  # Optional Docstring
-   print("Entering the start node")       # Optional messaging
-   DGW_state.the_answer = 1               # Do something to change data 
-   if DGW_state.the_answer :              # Use logic to determine next state 
-      operator = "stop"                   # operator is the next state
-   print("Next Operator is:", operator)   # Optional messaging
-   return(operator, DGW_state)            # Pass operator and modified state 
-                                          # back to DGWalker which will load
-                                          # and execute the next node.
+# Next we create the Graph Walker object that will hold our state methods
 
-def DGWOP_stop(DGW_state):
+DGW_Simple = pyDGW.DGWalker()            # Instantiate a DGWalker object
+
+# Next we define the operator functions for each state in our state machine
+# We use logic in each operator to determine which edge we will follow when
+# we change state. # An operator could contain another state machine.
+
+def OP_start(Simple_state):              # A Start Node is required
+   """Our start state"""                 # Optional Docstring
+   print("Entering the start node")      # Optional messaging
+   Simple_state.the_answer = 1           # Do something to change data 
+   if Simple_state.the_answer :          # Use logic to determine next state 
+      operator = "stop"                  # operator is the next state
+   print("Next Operator is:", operator)  # Optional messaging
+   return(operator, Simple_state)        # Pass operator and modified state 
+                                         # back to DGWalker which will load
+                                         # and execute the next node.
+
+def OP_stop(Simple_state):
    """Our stop state"""
-   DGW_state.the_answer = 42              # Do any final processing at shutdown
+   Simple_state.the_answer = 42        # Do any final processing at shutdown
    print("We have stopped")
-   return(DGW_state)                      # Returning our completed "output"
+   return(Simple_state)                # Returning our completed "output"
+
+# Finally we use the methods of pyDGW to build our state machine and run it
+
+DGW_Simple.DEBUG = True                # Setup any desired runtime options
+DGW_Simple.addNode("start", OP_start)  # Add a callback for each state node
+DGW_Simple.addNode("stop", OP_stop)
+DGW_Simple.setStartNode("start")       # Define ONE starting node
+DGW_Simple.setEndNode("stop")          # Define ONE or MORE ending nodes
+DGW_Simple.run(Simple_state)           # Run the initialized graph walker
 
 
-DGW_Simple.DEBUG = True                   # Setup any desired runtime options
-DGW_Simple.addNode("start", DGWOP_start)  # Add a callback for each state node
-DGW_Simple.addNode("stop", DGWOP_stop)
-DGW_Simple.setStartNode("start")          # Define ONE starting node
-DGW_Simple.setEndNode("stop")             # Define ONE or MORE ending nodes
-DGW_Simple.run(DGW_state)                 # Run the initialized graph walker
-
-
-# Now you can do additional post processing on DGW_state, generate other output
-# with matplotlib, and or format the information and print it on screen:
+# Now you can do additional post processing on Simple_state, generate other
+# output with matplotlib, and or format the information and print it on screen:
  
-print("The Answer to Life, The Universe and Everything Is: ", DGW_state.the_answer)
+print("The Answer to Life, The Universe and Everything Is: ", Simple_state.the_answer)
